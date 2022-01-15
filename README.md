a11ca6a196b4be0524512691bf87b562d0350823
David Denton <denton.david@gmail.com>
2022-01-15 21:18:27 +0000

    debug user directory

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 - https://monopolis-entry-logger.herokuapp.com/
 - https://monopolis-sso.herokuapp.com/
 - https://monopolis-user-directory.herokuapp.com/
-- https://monopolis-security-server.herokuapp.com/
+- https://monopolis-security-system.herokuapp.com/
--- user-directory/src/main/kotlin/verysecuresystems/UserDirectoryServer.kt
+++ user-directory/src/main/kotlin/verysecuresystems/UserDirectoryServer.kt
@@ -4,12 +4,13 @@ import org.http4k.cloudnative.env.Environment
 import org.http4k.cloudnative.env.Environment.Companion.ENV
 import org.http4k.cloudnative.env.EnvironmentKey
 import org.http4k.core.with
+import org.http4k.filter.debug
 import org.http4k.lens.int
 import org.http4k.server.Undertow
 import org.http4k.server.asServer
 import verysecuresystems.UserDirectorySettings.PORT
 
-fun UserDirectoryServer(env: Environment) = UserDirectory().asServer(Undertow(PORT(env)))
+fun UserDirectoryServer(env: Environment) = UserDirectory().debug().asServer(Undertow(PORT(env)))
 
 object UserDirectorySettings {
     val PORT = EnvironmentKey.int().required("PORT")
