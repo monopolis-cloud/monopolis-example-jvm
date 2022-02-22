0a0f815bf42d4aac65688483642fc614ffb9164c
albertlatacz <albert.latacz@gmail.com>
2022-02-22 17:53:25 +0000

    trigger build

--- sso/src/main/kotlin/verysecuresystems/UserAuthentication.kt
+++ sso/src/main/kotlin/verysecuresystems/UserAuthentication.kt
@@ -2,8 +2,6 @@ package verysecuresystems
 
 import org.http4k.core.Credentials
 
-
-
 class UserAuthentication(private vararg val validCredentials: Credentials) {
     fun authenticate(credentials: Credentials) = credentials in validCredentials
 }
